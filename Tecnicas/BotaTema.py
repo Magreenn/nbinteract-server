4ff6c698cc9309b69819b3331c6eb944dd2c545a
João Pedro Nunes Cavalcante <joao.pedro.nunes@poli.ufrj.br>
2021-01-04 12:06:54 -0300

    Adiciona título Nesta versão o programa muda o título para o nome final do arquivo

--- Tecnicas/BotaTema.py
+++ Tecnicas/BotaTema.py
@@ -1,11 +1,15 @@
 from bs4 import BeautifulSoup
 
+arquivo_inicial = input("Escreva o nome do arquivo fonte:")
+arquivo_final = input("Escreva o nome do arquivo de destino:")
+
 with open('Modelo.html', encoding="utf8") as modelo_file:
     modelo = BeautifulSoup(modelo_file, 'lxml')
-with open(input("Escreva o nome do arquivo base .html:"), encoding="utf8") as base_file:
+with open(f'{arquivo_inicial}.html', encoding="utf8") as base_file:
     base = BeautifulSoup(base_file, 'lxml')
     conteudo = base.body
+    titulo = modelo.title
+    titulo.string = arquivo_final
     modelo.html.append(conteudo)
-with open(input("Escreva o nome do arquivo final .html:"), encoding="utf8", mode="w") as final_file:
+with open(f'{arquivo_final}.html', encoding="utf8", mode="w") as final_file:
     final_file.write(modelo.prettify())
-
